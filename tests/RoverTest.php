0b588ce40c22a0f303d777ead6d3ad0a55d30f6e
Dick Brouwers <dick@insided.nl>
2018-05-09 13:24:53 +0200

    initial commit

---
+++ tests/RoverTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace unit\Dojo\MarsRover;
+
+use PHPUnit\Framework\TestCase;
+
+final class RoverTest extends TestCase
+{
+    public function testFoo()
+    {
+        self::assertTrue(true);
+    }
+}
+
